f1d53199413a400754f5eb06428542f9f48e9c3a
nv-vn <nv@cock.li>
2015-12-31 00:31:12 -0500

    Cleaned out old code

--- src/json_types/json_types.ml
+++ src/json_types/json_types.ml
@@ -16,9 +16,6 @@ type ty =
   | Product of ty list
   | Record of (string * ty) list
 
-type ct_or_tk =
-  Ct of core_type | Tk of type_kind
-
 let uniq =
   let x = ref 0 in
   fun start ->
@@ -32,39 +29,6 @@ let get_json url =
 let yj_to_string = function
   | `Null -> ""
   | any -> Yojson.Basic.Util.to_string any
-
-let make_record json loc =
-  let open Yojson.Basic.Util in
-  let items = to_list json in
-  let pairs = List.map (fun x -> (x |> member "name" |> to_string,
-                                  x |> member "type" |> to_string)) items in
-  Ptype_record (List.map (fun (name, typ) ->
-                           Type.field ~loc {txt = name; loc = loc}
-                             (Typ.constr ~loc {txt = Lident typ; loc = loc} [])) pairs)
-
-and make_product json loc =
-  let open Yojson.Basic.Util in
-  let items = to_list json |> filter_string in
-  Typ.tuple ~loc (List.map (fun item -> Typ.constr ~loc {txt = Lident item; loc = loc} []) items)
-
-and make_alias json loc =
-  let open Yojson.Basic.Util in
-  let name = to_string json in
-  Typ.constr ~loc {txt = Lident name; loc = loc} []
-
-let type_of_url url loc =
-  get_json url >>= fun text ->
-    let json = Yojson.Basic.from_string text in
-    (* Now process the JSON *)
-    let open Yojson.Basic.Util in
-    let record  = json |> member "record"
-    and product = json |> member "product"
-    and alias   = json |> member "alias" in
-    return @@ match (record, product, alias) with
-    | (record, `Null, `Null)  -> Tk (make_record  record  loc)
-    | (`Null, product, `Null) -> Ct (make_product product loc)
-    | (`Null, `Null, alias)   -> Ct (make_alias   alias   loc)
-    | (_, _, _) -> Ct (make_alias (`String "int") loc)
 
 let rec ty_of_json json =
   let open Yojson.Basic.Util in
@@ -157,24 +121,6 @@ let struct_of_url url loc =
 
 let rec json_type_mapper argv =
   { default_mapper with
-    type_declaration = begin fun mapper type_decl ->
-      match type_decl with
-      | { ptype_attributes; ptype_name; ptype_manifest = Some {
-            ptyp_desc = Ptyp_extension ({txt = "json_type"; loc}, pstr) } } ->
-        begin match pstr with
-        | PStr [{ pstr_desc =
-                  Pstr_eval ({ pexp_loc  = loc;
-                               pexp_desc = Pexp_constant (Const_string (sym, None))}, _)}] ->
-          begin match Lwt_unix.run @@ type_of_url sym loc with
-          | Ct ct -> {type_decl with ptype_manifest = Some ct}
-          | Tk tk -> {type_decl with ptype_kind = tk; ptype_manifest = None}
-          end
-        | _ ->
-          raise (Location.Error
-                  (Location.error ~loc "[%json_type ...] accepts a string, e.g. [%json_type \"http://google.com\"]"))
-        end
-      | x -> default_mapper.type_declaration mapper x
-    end;
     module_expr = begin fun mapper mod_expr ->
       match mod_expr with
       | { pmod_attributes; pmod_loc; pmod_desc = Pmod_extension ({txt = "json"; loc}, pstr) } ->
--- test/test_ppx_json_types.ml
+++ test/test_ppx_json_types.ml
@@ -1,14 +1,7 @@
-type vector3i = [%json_type "http://ix.io/n2h"]
-type char_ptr = [%json_type "http://ix.io/n2i"]
-type myrecord = [%json_type "http://ix.io/n2n"]
-
 module Conglomerate = [%json "http://ix.io/n4i"]
 
 let _ =
-  let vec : vector3i = (5, ~-1, 3)
-  and str : char_ptr = "memes"
-  and xyz : myrecord = {x = ~-1567; y = "stuff"}
-  and tup : Conglomerate.t = { xs = ["hello"; "world"]; ys = (5, 10) } in
+  let tup : Conglomerate.t = { xs = ["hello"; "world"]; ys = (5, 10) } in
   print_endline @@ Conglomerate.to_string tup;
   let serialized = Conglomerate.to_json tup in
   print_endline @@ Yojson.Safe.pretty_to_string serialized
